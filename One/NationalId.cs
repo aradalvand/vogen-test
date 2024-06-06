0ca57fabc0d25570392b60426b9d00a5779c4796
Arad Alvand <aradalvand@gmail.com>
2024-06-06 23:21:33 +0330

    Initial

---
+++ One/NationalId.cs
@@ -0,0 +1,15 @@
+﻿using Vogen;
+
+namespace One;
+
+[ValueObject<string>]
+public readonly partial struct NationalId
+{
+	private static Validation Validate(string input)
+	{
+		if (input.Length != 10)
+			return Validation.Invalid("A national ID must be 10 digits.");
+
+		return Validation.Ok;
+	}
+}
---
+++ Two/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using One;
+
+var foo = new Foo(NationalId.From("1234567890"));
+Console.WriteLine(
+	JsonSerializer.Serialize(
+		foo,
+		new JsonSerializerOptions { WriteIndented = true }
+	)
+);
+
+Console.WriteLine(
+	JsonSerializer.Serialize(
+		foo,
+		JsonContext.Default.Options
+	)
+);
+
+public record Foo(
+	NationalId NationalId
+);
+
+[JsonSerializable(typeof(Foo))]
+[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.KebabCaseLower)]
+public partial class JsonContext : JsonSerializerContext;
